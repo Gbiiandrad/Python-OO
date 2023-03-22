2ce5f8a8b40b000bf0062e631a830a44ed70e4cf
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-03-22 17:50:31 -0300

    Em andamento 🚧
    Usando "Propriedade", coisas acrescentadas:
    atibutos privados.
    Get e Set.
    Objeto @property.
    "title()" - deixando a primeira letra maiúscula do nome assim o nome não ficara com a letra minúscula.

--- Python_OO/conta.py
+++ Python_OO/conta.py
@@ -1,6 +1,7 @@
 class Conta:
 
     #para deixar o atributo privado usamos (__)
+    # O "self" irá ser a conta atual
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
         self.__numero = numero
@@ -15,3 +16,28 @@ class Conta:
 
     def saca(self, valor):
         self.__saldo -= valor
+
+    # um metodo - conta - valor a ser transferido - destino do dinheiro
+    #conta ( conta2 ) - valor para transferir ( R$ 10,00 ) - destino ( conta 1 )
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    # "get" para devolver/retornar o que é pedido ( saldo, titular, limite e etc...)
+    # "@property" se trata de uma propriedade então não precisa usar os () para chama-lo ELE É OS GET's
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    #"set" para receber/alterar algo
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
